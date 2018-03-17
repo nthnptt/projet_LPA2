ff6027b2c7ff7edce0725d00d0f0b004bfaf7c41
yaman0 <yaman0@protonmail.com>
2018-03-17 11:08:38 +0100

    Add test for town geocoding

--- test/models/town_test.rb
+++ test/models/town_test.rb
@@ -4,4 +4,20 @@ class TownTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "town validation does geocoding" do
+    belfort = Town.new
+    belfort.name = 'belfort'
+    belfort.postal_code = '90000'
+    assert belfort.valid?
+    assert_equal(47.6379599, belfort.lat)
+    assert_equal(6.8628942, belfort.long)
+  end
+
+  test "town not exists" do
+    unknown = Town.new
+    unknown.name = 'MyString'
+    assert !unknown.valid?
+  end
+
 end
