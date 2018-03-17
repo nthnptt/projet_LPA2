21d8015c0e25b046b30d75b4fc5da0600cfdb829
yaman0 <yaman0@protonmail.com>
2018-03-17 09:53:23 +0100

    Add method to get localisation of the city

--- app/models/town.rb
+++ app/models/town.rb
@@ -1,2 +1,14 @@
 class Town < ActiveRecord::Base
+    validates :name, :postal_code, presence: true
+    before_validation :getLocation
+
+  private
+  def getLocation
+    city = Nominatim.search(self.name).limit(1).first
+    if city
+      self.lat = city.lat
+      self.long = city.lon
+    end
+  end
+
 end
